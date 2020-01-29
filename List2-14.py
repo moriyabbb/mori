54d669145c420ec8df15f05543c8d3557bf10405
moriyabbb <c01172975e@edu.teu.ac.jp>
2020-01-29 15:24:51 +0900

    Python難しくね

---
+++ List2-14.py
@@ -0,0 +1,5 @@
+a = int(input('整数a : '))
+b = int(input('整数b : '))
+
+print('aとbの和は{}です'.format(a + b))
+print('{}と{}の和は{}です'.format(a , b ,a + b))
---
+++ List2-15.py
@@ -0,0 +1,2 @@
+name = input('お名前は?\n>')
+print('こんにちは{}さん'.format(name))
